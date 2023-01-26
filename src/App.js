1617a0d5e1d77f3f96d6cf7dfa786182a8382182
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-01-25 20:11:37 -0500

    setup css basics and READ.md

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import "./App.css";
-import pic from "./images/bg-card-back.png";
-
 function App() {
   return (
     <div className="App">
       <h3>My App</h3>
-      <h3>hello</h3>
-
-      <img src={pic} alt="pic" width={300} height={200} />
     </div>
   );
 }
